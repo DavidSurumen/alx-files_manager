e0ef1254880aaf42435da6ced034e71679ddacf9
David Surumen <davidsurumen@gmail.com>
2024-06-22 01:45:52 +0300

    refactored: Update route handling and logging - Refactored route handling to use root path '/' instead of '/api'. - Updated logging to correctly display the server port. - Switched from 'require' syntax to ES6 'import'.

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 import express from 'express';
-import router from './routes/index';
+import routes from './routes/index';
 
-const express = require('express');
 const app = express();
-const routes = require('./routes/index');
-
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use('/api', routes);
+app.use('/', routes);
 
-app.listen(port, () =>{
-    console.log('server listening on port ${port}');
+app.listen(port, () => {
+    console.log(`server listening on port ${port}`);
 });
 
 export default app;
